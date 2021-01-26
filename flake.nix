ee4bfd63a0aa0f8cc0aa66839ab5977321a34ecd
Blaž Hrastnik <blaz@mxxn.io>
2021-01-26 12:09:23 +0900

    Add a flake compatibility shim.

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "mobile-nixos";
+
+  outputs = inputs@{ self }:
+    let
+      all-devices =
+        builtins.filter
+        (d: builtins.pathExists (./. + "/devices/${d}/default.nix"))
+        (builtins.attrNames (builtins.readDir ./devices))
+      ;
+    in {
+      nixosModules = builtins.listToAttrs (map (device:
+        {
+          name = device;
+          value = {
+            imports = [
+              (import (./devices + "/${device}"))
+            ]
+            ++ import ./modules/module-list.nix;
+          };
+        }
+      ) all-devices);
+    };
+}
